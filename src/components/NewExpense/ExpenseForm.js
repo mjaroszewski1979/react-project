e0f0f0cf2cf7e255efcb4b27eebec8058f6a2c91
mjaroszewski <mjaroszewski1979@gmail.com>
2023-06-21 21:29:11 +0200

    added conditional rendering of expenses form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -34,8 +34,6 @@ const ExpenseForm = (props) => {
         setEnteredDate('');
     };
 
-
-
     return (
         <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
@@ -53,7 +51,7 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className='new-expense__actions'>
-                <button type='submit'>Cancel</button>
+                <button type="button" onClick={props.onCancel}>Cancel</button>
                 <button type='submit'>Add Expense</button>
             </div>
         </form>
