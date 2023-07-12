92e72ea3ef471278f22542fe966781d5100ef060
mjaroszewski <mjaroszewski1979@gmail.com>
2023-07-12 20:38:08 +0200

    added new component - ErrorModal.js

--- src/practice_02/src/components/Users/AddUser.js
+++ src/practice_02/src/components/Users/AddUser.js
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
 
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
 
     const usernameChangeHandler = (event) => {
         setEnteredUsername(event.target.value);
@@ -19,16 +21,31 @@ const AddUser = (props) => {
     const addUserHandler = (event) => {
         event.preventDefault();
         if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: 'An error occured',
+                message: 'Invalid input. Try again.',
+            });
             return;
         }
         if (+enteredAge < 1) {
+            setError({
+                title: 'An error occured',
+                message: 'Invalid age. Try again with greater than zero!',
+            });
             return;
         }
         props.onAddUser(enteredUsername, enteredAge);
         setEnteredUsername('');
         setEnteredAge('');
     };
+
+    const errorHandler = () => {
+        setError(null);
+    };
+
     return (
+        <div>
+        {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}></ErrorModal>}
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
@@ -38,7 +55,7 @@ const AddUser = (props) => {
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
-
+        </div>
     );
 
 };
