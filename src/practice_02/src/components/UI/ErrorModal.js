b168891dbcc6be88512aca5e701d89cc687e1cc0
mjaroszewski <mjaroszewski1979@gmail.com>
2023-07-14 21:07:21 +0200

    added Portals in ErrorModal.js file to ensure proper htm structure

--- src/practice_02/src/components/UI/ErrorModal.js
+++ src/practice_02/src/components/UI/ErrorModal.js
@@ -1,12 +1,17 @@
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
+import { Fragment } from "react";
+import reactDom from "react-dom";
 
-const ErrorModal = props => {
+const Backdrop = props => {
+    return <div className={classes.backdrop} onClick={props.onConfirm}></div>;
+
+};
+
+const ModalOverlay = props => {
     return (
-        <div>
-            <div className={classes.backdrop} onClick={props.onConfirm}></div>
-            <Card className={classes.modal}>
+        <Card className={classes.modal}>
                 <header>
                     <h2 className={classes.header}>{props.title}</h2>
                 </header>
@@ -16,9 +21,16 @@ const ErrorModal = props => {
                 <footer className={classes.actions}>
                     <Button onClick={props.onConfirm}>Send</Button>
                 </footer>
-            </Card>
-        </div>
+        </Card>
+    );
+};
 
+const ErrorModal = props => {
+    return (
+        <Fragment>
+            {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />, document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm}/>, document.getElementById('overlay-root'))}
+        </Fragment>
     )
 };
 
