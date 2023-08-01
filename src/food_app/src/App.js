73b7df873be4dce82d21f6ccdc0028d7eba05b25
mjaroszewski <mjaroszewski1979@gmail.com>
2023-08-01 20:17:53 +0200

    added state management functionality in App.js file to provide an availibilty for Cart component to be open and close

--- src/food_app/src/App.js
+++ src/food_app/src/App.js
@@ -1,17 +1,28 @@
-import React from "react";
+import { Fragment, useState} from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+
   return (
-    <React.Fragment>
-      <Cart></Cart>
-      <Header/>
+    <Fragment>
+      {cartIsShown && <Cart onHideCart={hideCartHandler}></Cart>}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals></Meals>
       </main>
-    </React.Fragment>
+    </Fragment>
   );
 }
 
--- src/food_app/src/components/Layout/Header.js
+++ src/food_app/src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props => {
         <React.Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <HeaderCartButton></HeaderCartButton>
+                <HeaderCartButton onClick={props.onShowCart}></HeaderCartButton>
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt="Table with meals"></img>
