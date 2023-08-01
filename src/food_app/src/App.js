f9b7ed90d0379951608b274e1fb853f2e9ed7850
mjaroszewski <mjaroszewski1979@gmail.com>
2023-08-01 20:44:00 +0200

    added context object to ensure access to all items acroos entire application

--- src/food_app/src/App.js
+++ src/food_app/src/App.js
@@ -1,7 +1,8 @@
-import { Fragment, useState} from "react";
+import { useState} from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -16,13 +17,13 @@ function App() {
 
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onHideCart={hideCartHandler}></Cart>}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals></Meals>
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- src/food_app/src/components/Layout/HeaderCartButton.js
+++ src/food_app/src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,17 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = props => {
+
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce(
+        (currNum, item) => {
+            return currNum + item.amount;
+        }, 0
+    );
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
@@ -9,7 +19,7 @@ const HeaderCartButton = props => {
             </span>
             <span>Your Cart</span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     )
