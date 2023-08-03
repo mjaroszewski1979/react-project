3eff204381b2426cc5f3b37b7d351f8f167d05b5
mjaroszewski <mjaroszewski1979@gmail.com>
2023-08-03 21:58:42 +0200

    added new functionality to Cart component - outputting amount of items added in MealItemForm component using forward Refs

--- src/food_app/src/components/Meals/MealItem/MealItemForm.js
+++ src/food_app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,29 @@
+import { useRef, useState } from 'react';
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
 
 const MealItemForm = props => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+    const submitHandler = event => {
+        event.preventDefault();
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if (
+            enteredAmount.trim().length === 0 || 
+            enteredAmountNumber < 1 || 
+            enteredAmountNumber < 5) {
+            setAmountIsValid(false);
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNumber);
+    };
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <Input
+            ref= {amountInputRef}
             label="Amount"
             input={{
                 id: 'amount_' + props.id,
@@ -16,8 +35,9 @@ const MealItemForm = props => {
             }}>
             </Input>
             <button>+ Add</button>
+            {!amountIsValid && <p>Invalid amount</p>}
         </form>
-    )
+    );
 };
 
 export default MealItemForm;
