d0d943ec43c5a25d70ac41c84e1782107cec55ee
mjaroszewski <mjaroszewski1979@gmail.com>
2023-08-08 20:57:08 +0200

    added error checking logic using try/catch block

--- src/star_wars_app/src/App.js
+++ src/star_wars_app/src/App.js
@@ -5,10 +5,20 @@ import './App.css';
 
 function App() {
   const [ movies, setMovies ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [ error, setError ] = useState(null);
+
   async function fetchMoviesHandler() {
-    const response = await fetch('https://swapi.dev/api/films');
-    const data = await response.json();
-    
+    setError(null);
+    setIsLoading(true);
+    try {
+      const response = await fetch('https://swapi.dev/api/film');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+
       const transformedMovies = data.results.map(movieData => {
         return {
           id: movieData.episode_id,
@@ -18,6 +28,25 @@ function App() {
         };
       });
       setMovies(transformedMovies);
+    } catch(error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+    
+  }
+
+  let content = <p>No movies yet.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = {error};
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -26,7 +55,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {content}
       </section>
     </React.Fragment>
   );
