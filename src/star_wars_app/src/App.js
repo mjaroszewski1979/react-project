1968133499dc1e7814528e74946e10683d720c20
mjaroszewski <mjaroszewski1979@gmail.com>
2023-08-09 18:54:47 +0200

    added useEffect hook to enable fetching data at the very first time the App component is being rendered, also wrapped async function with useCallback hook to prevent it from reloading upon each state change which could lead to infinite loop

--- src/star_wars_app/src/App.js
+++ src/star_wars_app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,11 +8,11 @@ function App() {
   const [ isLoading, setIsLoading ] = useState(false);
   const [ error, setError ] = useState(null);
 
-  async function fetchMoviesHandler() {
+ const fetchMoviesHandler = useCallback(async () => {
     setError(null);
     setIsLoading(true);
     try {
-      const response = await fetch('https://swapi.dev/api/film');
+      const response = await fetch('https://swapi.dev/api/films');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
@@ -33,7 +33,11 @@ function App() {
     }
     setIsLoading(false);
     
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>No movies yet.</p>;
 
