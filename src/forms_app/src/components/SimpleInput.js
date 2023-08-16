6c40a1e31975e3aa9deb56ca2f50f570c6346875
mjaroszewski <mjaroszewski1979@gmail.com>
2023-08-16 20:14:09 +0200

    added basic validation logic in SimpleInput component file

--- src/forms_app/src/components/SimpleInput.js
+++ src/forms_app/src/components/SimpleInput.js
@@ -3,27 +3,36 @@ import { useState, useRef } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
   };
   const formSubmissionHandler = event => {
     event.preventDefault();
-    const enteredValue = nameInputRef.current.value;
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
+    //const enteredValue = nameInputRef.current.value;
     setEnteredName('');
 
   }
 
+  const inputNameClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
+      <div className={inputNameClasses}>
         <label htmlFor='name'>Your Name</label>
         <input 
           type='text' 
           id='name' 
           ref={nameInputRef}
           onChange={nameInputChangeHandler}
-          onVolumeChangeCapture={enteredName}
+          value={enteredName}
           />
+        {!enteredNameIsValid && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
