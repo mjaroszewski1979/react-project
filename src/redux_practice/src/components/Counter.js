74816c9c983801757a88a156c3355fed839e091b
mjaroszewski <mjaroszewski1979@gmail.com>
2023-09-12 21:41:24 +0200

    added new export const in store/index.js file - counterActions to connect slice object with different handlers in counter.js file

--- src/redux_practice/src/components/Counter.js
+++ src/redux_practice/src/components/Counter.js
@@ -1,21 +1,22 @@
 import classes from './Counter.module.css';
 import { useSelector, useDispatch } from 'react-redux'
+import { counterActions } from '../store/index';
 
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
   const show = useSelector(state => state.showCounter);
   const incrementHandler = () => {
-    dispatch({ type: 'increment'});
+    dispatch(counterActions.increment());
   };
   const increaseHandler = () => {
-    dispatch({ type: 'increment', amount: 5});
+    dispatch(counterActions.increase(5)); // { type: SOME_UNIQUE_IDENTIFIER, payload: 5 }
   };
   const decrementHandler = () => {
-    dispatch({ type: 'decrement'});
+    dispatch(counterActions.decrement());
   };
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle'});
+    dispatch(counterActions.toggle());
   };
 
   return (
