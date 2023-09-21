45362a8d51a9f56bbcab949086332704b92f55bf
mjaroszewski <mjaroszewski1979@gmail.com>
2023-09-21 20:38:07 +0200

    added useEffect hook in App component to allow for sending cart related data into backend server right after every redux store update

--- src/redux_toolkit/src/App.js
+++ src/redux_toolkit/src/App.js
@@ -1,10 +1,16 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible)
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch('firebase-url', { method: 'PUT', body: JSON.stringify(cart)})
+  }, [cart]);
   return (
     <Layout>
       {showCart && <Cart />}
