e3b7c0ec4e63fe7d56cda33904e7be03c05b8ade
mjaroszewski <mjaroszewski1979@gmail.com>
2023-10-03 20:58:49 +0200

    added new events object fetched from backend server using useLoaderData hook from reacr-router-dom

--- src/router_advanced/frontend/src/pages/EventsPage.js
+++ src/router_advanced/frontend/src/pages/EventsPage.js
@@ -1,22 +1,12 @@
-import { Link } from "react-router-dom";
-
-const EVENTS = [
-    { id: 'e1', title: 'event1'},
-    { id: 'e2', title: 'event2'},
-    { id: 'e3', title: 'event3'}
-]
+import EventsList from '../components/EventsList';
 
 function EventsPage() {
-    return (
-        <>
-        <h1>Events Page</h1>
-        <ul>
-            {EVENTS.map((event) => (
-                <li key={event.id}><Link to={`/events/${event.id}`}>{event.title}</Link></li>
-            ))}
-        </ul>
-        </>
-    );
-};
+
+  return (
+    <>
+      <EventsList/>
+    </>
+  );
+}
 
 export default EventsPage;
