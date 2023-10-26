e93c580d6997a1f7e88ab2a06bbf23f4b10c27b9
mjaroszewski <mjaroszewski1979@gmail.com>
2023-10-26 20:35:28 +0200

    added route protection mechanism in a form of loader function that checks if authorization token is present

--- src/auth_app/frontend/src/App.js
+++ src/auth_app/frontend/src/App.js
@@ -15,12 +15,16 @@ import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 import AuthenticationPage, { action as authAction } from './pages/Authentication';
 import { action as logoutAction } from './pages/Logout'
+import { tokenLoader, checkAuthLoader } from './util/auth';
+
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: 'root',
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
@@ -51,6 +55,7 @@ const router = createBrowserRouter([
                 path: 'edit',
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loader: checkAuthLoader,
               },
             ],
           },
@@ -58,6 +63,7 @@ const router = createBrowserRouter([
             path: 'new',
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader: checkAuthLoader,
           },
         ],
       },
